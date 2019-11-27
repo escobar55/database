656384221aae0f76d15f84cb3cb5967a48d4bc4c
escobar55 <escobar5@umbc.edu>
2019-11-27 13:23:21 -0500

    BD

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashSet;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    ActorRepository actorRepository;
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        Actor actor = new Actor();
+        actor.setName("Sandra Bullock");
+        actor.setRealname("Sandra Mae Bullowski");
+
+        Movie movie = new Movie();
+        movie.setTitle("Emoji Movie");
+        movie.setYear(2017);
+
+        movie.setDescription("About Emojies...");
+        Set<Movie> movies new HashSet<Movie>();
+        movies.add(movie);
+    }
+}
